e05fcfe085744edfcdbd5e458e27a1eba44d5742
Eric Grossman <eric.josh.grossman@gmail.com>
2020-01-14 15:01:42 -0500

    template working

--- client/app.js
+++ client/app.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
 import {Navbar} from './components'
+import Home from './components/home'
 import Routes from './routes'
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
+    <React.Fragment>
+      <Home />
+
       <Routes />
-    </div>
+    </React.Fragment>
   )
 }
 
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,32 +1,60 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Responsive,
+  Segment,
+  Sidebar,
+  Visibility
+} from 'semantic-ui-react'
 import {logout} from '../store'
+import styled, {keyframes} from 'styled-components'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>BOILERMAKER</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-)
+const Navbar = props => {
+  return (
+    <Menu
+      items={['Home', 'About', 'Knot List'].map((c, i) => {
+        return {key: i + 1, name: c, content: 'Link'}
+      })}
+      point
+      secondary
+    />
+  )
+}
+// const Navbar = ({handleClick, isLoggedIn}) => (
+//   <div>
+
+//     <h1>KnotJam</h1>
+//     <nav>
+//       {isLoggedIn ? (
+//         <div>
+//           {/* The navbar will show these links after you log in */}
+//           <Link to="/home">Home</Link>
+//           <a href="#" onClick={handleClick}>
+//             Logout
+//           </a>
+//         </div>
+//       ) : (
+//         <div>
+//           {/* The navbar will show these links before you log in */}
+//           <Link to="/login">Login</Link>
+//           <Link to="/signup">Sign Up</Link>
+//         </div>
+//       )}
+//     </nav>
+//     <hr />
+//   </div>
+// )
 
 /**
  * CONTAINER
@@ -50,7 +78,7 @@ export default connect(mapState, mapDispatch)(Navbar)
 /**
  * PROP TYPES
  */
-Navbar.propTypes = {
-  handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
-}
+// Navbar.propTypes = {
+//   handleClick: PropTypes.func.isRequired,
+//   isLoggedIn: PropTypes.bool.isRequired
+// }
